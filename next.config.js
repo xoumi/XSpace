cb88175729efa94f3fddd0c1603a46e360dae377
xoumi <xoumi712@gmail.com>
2021-10-05 09:18:15 +0530

    feat: add mdx support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
+const withMDX = require('@next/mdx')()
+module.exports = withMDX()
 
-module.exports = {
+module.exports = withMDX({
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   }
-}
+})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
 import type { ReactElement } from 'react'
+import Link from 'next/link'
 
 const Landing: NextPage = (): ReactElement => {
   return (
-    <h2> Swoooosh, touchdown! </h2>
+    <>
+      <h2> Swoooosh, touchdown! </h2>
+      <Link href='/test'> To Test </Link>
+    </>
   )
 }
 
