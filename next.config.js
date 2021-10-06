f0ae2c02aef9a5bd0c1bf5aa97fdec53490932f0
xoumi <xoumi712@gmail.com>
2021-10-06 10:01:44 +0530

    refactor: move to mdx-bundler

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
-const withMDX = require('@next/mdx')()
-module.exports = withMDX()
 
-module.exports = withMDX({
-  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+module.exports = {
+  pageExtensions: ['ts', 'tsx'],
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   }
-})
+}
